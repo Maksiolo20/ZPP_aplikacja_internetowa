e9196740f25c23db0ae3e11be7f7d29385a79f24
Maksiolo20 <maksiolo2000@interia.pl>
2022-03-04 21:03:22 +0100

    Project init

---
+++ ZPP_aplikacja_internetowa/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ZPP_aplikacja_internetowa.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
