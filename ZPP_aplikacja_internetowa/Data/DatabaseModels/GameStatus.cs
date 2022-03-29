d895eb39984cbdfa51039e7aaedc55f9bed73a2a
Maksiolo20 <maksiolo2000@interia.pl>
2022-03-29 23:55:24 +0200

    Update GameStatus.cs

--- ZPP_aplikacja_internetowa/Data/DatabaseModels/GameStatus.cs
+++ ZPP_aplikacja_internetowa/Data/DatabaseModels/GameStatus.cs
@@ -3,6 +3,7 @@
     public class GameStatus
     {
         public int GameStatusId { get; set; }
+        public string StatusName { get; set; }
         public ICollection<Game> Games { get; set; }
     }
 }
