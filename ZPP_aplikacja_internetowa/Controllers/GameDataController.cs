565974dcabedcd3f2e8e7004f5643e11e6b475a0
Maksiolo20 <maksiolo2000@interia.pl>
2022-04-27 09:21:09 +0200

    Added (again) json deserializer

--- ZPP_aplikacja_internetowa/Controllers/GameDataController.cs
+++ ZPP_aplikacja_internetowa/Controllers/GameDataController.cs
@@ -1,12 +1,24 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
+using ZPP_aplikacja_internetowa.Data;
+using ZPP_aplikacja_internetowa.Data.DatabaseModels;
 
 namespace ZPP_aplikacja_internetowa.Controllers
 {
+    
     public class GameDataController : Controller
     {
-        public IActionResult Index()
+        private readonly ApplicationDbContext _context;
+        public GameDataController(ApplicationDbContext context)
         {
-            return View();
+            _context = context;
+        }
+        [HttpPost]
+        public IActionResult Index(string resultJson)
+        {
+            Game game = JsonSerializer.Deserialize<Game>(resultJson);
+            _context.Games.Add(game);
+            _context.SaveChangesAsync();
         }
     }
 }
