eb1bb4cda1b3b47915644a7f9ae7df09a549d2a5
Maksiolo20 <maksiolo2000@interia.pl>
2022-05-23 14:27:58 +0200

    Added Hall of Fame Controller and View

---
+++ ZPP_aplikacja_internetowa/Controllers/HallOfFameController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ZPP_aplikacja_internetowa.Data;
+using ZPP_aplikacja_internetowa.Data.DatabaseModels;
+
+namespace ZPP_aplikacja_internetowa.Controllers
+{
+    public class HallOfFameController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        public Game Model { get; set; }
+        public HallOfFameController(ApplicationDbContext context)
+        {
+            _context = context;
+            Model = new Game();
+        }
+        public IActionResult Index()
+        {
+            return View(Model);
+        }
+    }
+}
--- ZPP_aplikacja_internetowa/Data/Migrations/20220523120451_zpp.Designer.cs
+++ ZPP_aplikacja_internetowa/Data/Migrations/20220523120451_zpp.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using ZPP_aplikacja_internetowa.Data;
 
@@ -11,9 +12,10 @@ using ZPP_aplikacja_internetowa.Data;
 namespace ZPP_aplikacja_internetowa.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220523120451_zpp")]
+    partial class zpp
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
