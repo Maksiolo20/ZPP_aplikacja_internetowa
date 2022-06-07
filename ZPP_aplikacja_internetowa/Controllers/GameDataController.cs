169d524f022d24d90de5657097a5604533c7c584
Maksiolo20 <maksiolo2000@interia.pl>
2022-06-07 20:45:55 +0200

    Update GameDataController.cs

--- ZPP_aplikacja_internetowa/Controllers/GameDataController.cs
+++ ZPP_aplikacja_internetowa/Controllers/GameDataController.cs
@@ -13,49 +13,23 @@ namespace ZPP_aplikacja_internetowa.Controllers
     [Route("Api")]
     public class GameDataController : Controller
     {
-        private readonly ApplicationDbContext _context;
-        private readonly IAuthentication _authentication;
-        private readonly ILogger<GameDataController> _logger;
+        private readonly IGameData _gameData;
 
-        public GameDataController(ApplicationDbContext context, IAuthentication authentication, ILogger<GameDataController> logger)
+        public GameDataController(IGameData gameData)
         {
-            _context = context;
-            _authentication = authentication;
-            _logger = logger;
-            _logger.LogWarning($"users {context.Users.Count()}");
+            _gameData = gameData;
         }
 
         [HttpPost("Login")]
         public async Task<IActionResult> Login([FromForm] UnityUser user)
         {
-            _logger.LogInformation($"Tryin to log user: {user.Email}, pass: {user.Password}");
-            var toLog = _context.Users.FirstOrDefault(x => x.Email == user.Email);
-            if (toLog is not null)
-            {
-                var result = await _authentication.Login(toLog, user.Password);
-                return Ok(result);
-            }
-            return BadRequest();
+            return await _gameData.Login(user) ? Ok() : BadRequest();
         }
 
         [HttpPost("GetWinner")]
         public async Task<IActionResult> RecieveStats([FromForm] UnityGameResult unityGameResult)
         {
-            Game plyedGame = new() { GameStatusId = 2, MapId = unityGameResult.MapId};
-            //Game game = JsonSerializer.Deserialize<Game>(resultJson);
-            if (unityGameResult.WinnersEmail != null)
-            {
-                string winnerId = _context.Users.FirstOrDefault(r => r.Email == unityGameResult.WinnersEmail).Id;
-                plyedGame.WinnerId = winnerId;
-            }
-            foreach (var item in unityGameResult.PlayersEmails)
-            {
-                var player = _context.Users.FirstOrDefault(r => r.Email == item);
-                _context.GameUsers.Add(new GameUser { UserId = player.Id, GameId = plyedGame.GameId });
-            }
-            _context.Games.Add(plyedGame);
-            _context.SaveChanges();
-            return Ok();
+            return await _gameData.RecieveStats(unityGameResult) ? Ok() : BadRequest();
         }
     }
 }
