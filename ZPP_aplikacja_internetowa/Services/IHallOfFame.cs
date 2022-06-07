4bba8f87abe3dc4ab289b76b041fb101f013552f
Maksiolo20 <maksiolo2000@interia.pl>
2022-06-07 20:45:46 +0200

    Create IHallOfFame.cs

---
+++ ZPP_aplikacja_internetowa/Services/IHallOfFame.cs
@@ -0,0 +1,9 @@
+﻿using ZPP_aplikacja_internetowa.Models;
+
+namespace ZPP_aplikacja_internetowa.Services
+{
+    public interface IHallOfFame
+    {
+        public List<HallOfFameViewModel> GetGames();
+    }
+}
