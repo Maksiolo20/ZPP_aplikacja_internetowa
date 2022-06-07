347e39457ca89f1fefee2e2bbcd23abf1d046689
Maksiolo20 <maksiolo2000@interia.pl>
2022-06-07 20:45:44 +0200

    Create IGameData.cs

---
+++ ZPP_aplikacja_internetowa/Services/IGameData.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ZPP_aplikacja_internetowa.Data.DatabaseModels;
+using ZPP_aplikacja_internetowa.Models;
+
+namespace ZPP_aplikacja_internetowa.Services
+{
+    public interface IGameData
+    {
+        public Task<bool> Login([FromForm] UnityUser user);
+        public Task<bool> RecieveStats([FromForm] UnityGameResult unityGameResult);
+    }
+}
